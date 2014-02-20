b00ee9a2832f3deb64df1fb343d2ea2883d5cc37
David Thompson <dt@eiger-tech.com>
2014-02-19 20:20:04 -0800

    halfway to 9, ish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    #@user = User.find(params[:id]) #not needed bc of before_action
+  end
+
+  def update
+    #@user = User.find(params[:id]) #not needed bc of before_action
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def new
   	@user = User.new
   end
@@ -26,4 +45,14 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+# Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
